13b5547abb552e8d83e71b8f4dc7cf08f39c912e
Cleiton <cleitonwoycik@outlook.com>
2022-02-08 21:43:58 -0300

    refactor: rename Encrypter to Hasher

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -9,9 +9,9 @@ import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
-  const encrypter = new BcryptAdapter(salt)
+  const hasher = new BcryptAdapter(salt)
   const addAccountRepository = new AccountMongoRepository()
-  const addAccount = new DbAddAccount(encrypter, addAccountRepository)
+  const addAccount = new DbAddAccount(hasher, addAccountRepository)
   const signUpController = new SignUpController(addAccount, makeSignUpValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
