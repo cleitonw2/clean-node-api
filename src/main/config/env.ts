8600ec58ac1508d56c7857717a82f068ee87ea34
Cleiton <cleitonwoycik@outlook.com>
2022-02-09 22:33:51 -0300

    feat: add LoginFactory method

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://mongodb:docker@localhost:27017/clean-api?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || 'a12bBjdjewjmdqio7wruqriofacZsolç3'
 }
