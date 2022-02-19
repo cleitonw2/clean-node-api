8767ae575699b6742285103516ee075c64b3ab7a
Cleiton <cleitonwoycik@outlook.com>
2022-02-19 12:56:02 -0300

    test: ensure DbLoadSurveyById returns a Survey on succeeds

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -51,4 +51,10 @@ describe('DbLoadSurveyById', () => {
     await sut.loadById('any_id')
     expect(loadByidSpy).toBeCalledWith('any_id')
   })
+
+  test('Should return Survey on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.loadById('any_id')
+    expect(surveys).toEqual(makeFakeSurvey())
+  })
 })
