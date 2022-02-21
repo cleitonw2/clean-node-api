bce6909aa7b503c121cb21cd33df53899fbac296
Cleiton <cleitonwoycik@outlook.com>
2022-02-21 14:26:06 -0300

    test: ensure save survey result router returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,7 +1,63 @@
 import request from 'supertest'
+import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
 import app from '@/main/config/app'
+import env from '@/main/config/env'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'valid_email@mail.com',
+    password: 'any_password'
+  })
+  const accessToken = sign({ id: res.insertedId.toHexString() }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: res.insertedId
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
+
+const makeSurveyId = async (): Promise<string> => {
+  const result = await surveyCollection.insertOne({
+    question: 'new Question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'answer_1'
+      },
+      {
+        answer: 'answer_2'
+      }
+    ],
+    date: new Date()
+  })
+  return result.insertedId.toHexString()
+}
 
 describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.geCollection('surveys')
+    accountCollection = await MongoHelper.geCollection('accounts')
+    await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
+  })
+
   describe('PUT /surveys/:suveyId/results', () => {
     test('Should return 403 if save survey result without accessToken', async () => {
       const response = await request(app)
@@ -10,6 +66,17 @@ describe('Survey Routes', () => {
           answer: 'any_answer'
         })
       expect(response.status).toEqual(403)
+    })
+
+    test('Should return 200 on save survey result succeeds', async () => {
+      const accessToken = await makeAccessToken()
+      const surveyId = await makeSurveyId()
+      await request(app)
+        .put(`/api/surveys/${surveyId}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'answer_1'
+        }).expect(200)
     })
   })
 })
