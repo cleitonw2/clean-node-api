0e23f2401357aabb3e9e464124f0e4b0060e2211
Cleiton <cleitonwoycik@outlook.com>
2022-02-23 11:59:53 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,12 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Enquetes para Programadores',
+    description: 'Essa é a documentação da Api construida usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
+    version: '3.0.0',
+    contact: {
+      name: 'Cleiton',
+      email: 'cleitonwoycik@outlook.com'
+    }
+  }
+}
