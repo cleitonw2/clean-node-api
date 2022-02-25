11f4e9a34ecf9e41b222a7e6d1fc3b37a37185d5
Cleiton <cleitonwoycik@outlook.com>
2022-02-25 16:47:57 -0300

    fix: fix bug return save survey result

--- tests/infra/db/mongodb/survey-result-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey-result-mongo-repository.spec.ts
@@ -55,6 +55,8 @@ describe('SurveyResultMongo Repository', () => {
       expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
 
     test('Should update a survey result if already exists', async () => {
@@ -78,6 +80,8 @@ describe('SurveyResultMongo Repository', () => {
       expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer)
       expect(surveyResult.answers[0].count).toBe(1)
       expect(surveyResult.answers[0].percent).toBe(100)
+      expect(surveyResult.answers[1].count).toBe(0)
+      expect(surveyResult.answers[1].percent).toBe(0)
     })
   })
 })
